87930c2ad83e63e1f6d495cb0fed313778343a41
rinufathima-doc <rinufathimadoc@gmail.com>
2026-09-24 09:59:04 +0530

    updated product backend

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 import HttpError from "../helpers/httpError.js";
-import User from "../models/user.js";
+import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import { validationResult } from "express-validator";
